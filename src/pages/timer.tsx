55c646e9f4c7368b1e22f34fd41a32a3fa960203
Olga <helgastogova@gmail.com>
2025-02-25 20:06:09 +0100

    for Ira, timer

---
+++ src/pages/timer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button, Text } from "@ui";
+import { Timer } from "@client/timer";
+
+const TimerPage = () => {
+  return <Timer />;
+};
+
+export default TimerPage;
